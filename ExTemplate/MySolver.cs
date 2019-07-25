78abf4bcedebe1fd24cfcd80761170dd76d64b2f
protarie125 <proarie@gmail.com>
2019-07-25 21:15:16 +0900

    기본 구조

---
+++ ExTemplate/MySolver.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExTemplate
+{
+    public class MySolver
+    {
+        public MySolver()
+        {
+            // TODO: 여기에 초기화 코드를 작성합니다.
+        }
+
+        public void Run()
+        {
+            // TODO: 여기에 코드를 작성합니다.
+        }
+    }
+}
---
+++ InterfaceConsole/MainApp.cs
@@ -0,0 +1,23 @@
+﻿using ExTemplate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceConsole
+{
+    static class MainApp
+    {
+        static void Main(string[] args)
+        {
+            var solver = new MySolver();
+
+            // TODO: 여기에 데이터 인풋 코드를 작성합니다.
+
+            solver.Run();
+
+            // TODO: 여기에 결과 아웃풋 코드를 작성합니다.
+        }
+    }
+}
