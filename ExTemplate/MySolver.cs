e3fde172ca4f6f0a41f54bbb4c0d1acf54e9b70b
protarie125 <proarie@gmail.com>
2019-07-25 22:23:10 +0900

    구성 변경
    기본적인 인풋/아웃풋 루틴을 추가하였습니다.

--- ExTemplate/MySolver.cs
+++ ExTemplate/MySolver.cs
@@ -8,14 +8,22 @@ namespace ExTemplate
 {
     public class MySolver
     {
+        // TODO: Data의 형식을 적절하게 바꿉니다.
+        public int Data { get; set; }
+        // TODO: Result의 형식을 적절하게 바꿉니다.
+        public int Result { get; set; }
+
         public MySolver()
         {
-            // TODO: 여기에 초기화 코드를 작성합니다.
+            // TODO: 여기에 인스턴스를 초기화하는 코드를 작성합니다.
+            Data = 0;
+            Result = 0;
         }
 
         public void Run()
         {
-            // TODO: 여기에 코드를 작성합니다.
+            // TODO: 여기에 필요한 코드를 작성합니다.
+            Result = Data;
         }
     }
 }
--- InterfaceConsole/MainApp.cs
+++ InterfaceConsole/MainApp.cs
@@ -1,6 +1,7 @@
 ﻿using ExTemplate;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,11 +14,24 @@ namespace InterfaceConsole
         {
             var solver = new MySolver();
 
-            // TODO: 여기에 데이터 인풋 코드를 작성합니다.
+            // TODO: 여기에 인풋 코드를 작성합니다.
+            var input = new FileInfo(@"..\..\input.txt");
+            var fs = input.Open(FileMode.Open);
+            var sr = new StreamReader(fs);
 
-            solver.Run();
+            for (var i = 0; i < 3; i++)
+            {
+                solver.Data = int.Parse(sr.ReadLine());
 
-            // TODO: 여기에 결과 아웃풋 코드를 작성합니다.
+                solver.Run();
+
+                // TODO: 여기에 결과를 콘솔에 출력하기 위한 코드를 작성합니다.
+                Console.WriteLine(solver.Result);
+            }
+
+            sr.Close();
+            fs.Close();
+
         }
     }
 }
